480bb147627c59b6dece1b1ef2bf5a7cbcebf1a3
nalchevanidze <d.nalchevanidze@gmail.com>
2018-02-08 23:23:08 +0100

    feature(wave_looper-yp) note_to_frequency

--- wave_looper.py
+++ wave_looper.py
@@ -1,16 +1,22 @@
 import math;
 from audio_config import SAMPLE_RATE;
 
-class WaveLooper:
-    def __init__(self, note = 0):
-        self.state = 0;
-        self.waveSize =(math.pi * 2);
-        self.set(440 + note * 64 )
+def note_to_frequency(index):
+    power_value = (index - 49) / 12
+    return (2 ** power_value) * 440
 
-    def set(self, frequency = 440 ):
-        self.frequency = frequency;
-        self.stepSize = float(frequency) / (SAMPLE_RATE);
+
+class WaveLooper:
+    def __init__(self, note=1):
+        self.state = 0
+        self.waveSize =(math.pi * 2)
+        self.set(note)
+
+    def set(self, note=1):
+        frequency = note_to_frequency(note + 36)
+        self.frequency = frequency
+        self.step_size = float(frequency) / SAMPLE_RATE
 
     def next(self):
-        self.state  = (self.state + self.stepSize) % self.waveSize;
-        return self.state;
+        self.state = (self.state + self.step_size) % self.waveSize
+        return self.state
